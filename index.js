6ff8c3dbc7f54594dce58cd81b6f80658dd4a666
Júlio César Barcelo Monteiro <julczar21@outlook.com>
2020-04-11 17:56:14 -0300

    Added Url Parser

--- index.js
+++ index.js
@@ -2,90 +2,42 @@
  * JulCzar javascript framework
  * Made by Júlio César Barcelo Monteiro
  * Date: 12/19/2018
- * Last Edit: 01/09/2019
+ * Last Edit: 04/11/2019
  */
 
-const d = document; // caminho direto para o DOM;
+const d = document;
 
-//Objeto que contêm métodos muito utilizados de obter dados da página ou navegador
 const get = {
-
-    /**
-     * Método que retorna um elemento da página com base em um  Id selecionado pelo usuario
-     * em forma de String.
-     * @param { <tag id="item"></tag> } item tag em forma de String, será utilizada no método
-     *                                       para obtenção de dados no DOM.
-     */
     Id(item) {
         return d.getElementById(item);
     },
-
-
-    /**
-     * Método que retorna um arrays de elementos da página que coincidirem com a classe
-     * selecionada pelo usuario em forma de String.
-     * @param { [<tag class="item"></tag>, ...] } item Classe em forma de String, será
-     *                               utilizada no método para obtenção de dados no DOM.
-     */
     Classes(item) {
         return d.getElementsByClassName(item);
     },
-
-
-    /**
-     * Método que retorna o primeiro elemento da página que coincidir com o query selecionado
-     * pelo usuario em forma de String. Pode receber Id's e Classes, porém com os respectivos
-     * prefixos de cada um.
-     * @param {<item class="item" "id="item"></item>} item query em
-     *                 forma de String, será utilizada no método para obtenção de dados no DOM.
-     */
     Query(item) {
         return d.querySelector(item);
     },
-
-
-    /**
-     * Método que retorna um arrays de elementos da página que coincidirem com a query
-     * selecionada pelo usuario em forma de String.  Pode receber Classes, porém com o 
-     * prefixo.
-     * @param {[<item class="item" id="item"></item>], ...} item query em forma de String,
-     *                             será utilizada no método para obtenção de dados no DOM.
-     */
     Queries(item) {
         return d.querySelectorAll(item);
     },
-
-
-    /**
-     * Método que retorna a data atual em milissegundos.
-     */
     Date() {
         return new Date().getTime();
     },
-
-
-    /**obtém de dados das API's de armazenamento imbutidas no
-     * navegador: localStorage e sessionStorage.
-     * 
-     * @param { [...] ou {...} } id Identificador salvo na sessionStorage
-     */
     Session(id) {
         return JSON.parse(sessionStorage.getItem(id));
     },
-
-
-    /**obtém de dados das API's de armazenamento imbutidas no
-     * navegador: localStorage e sessionStorage.
-     * 
-     * @param { [...] ou {...} } id identificador salvo na localStorage
-     */
     Local(id) {
         return JSON.parse(localStorage.getItem(id));
+    },
+    UrlData() {
+        let urlData = decodeURIComponent(location.search.split('?')[1])
+        urlData = urlData.replace(/=/g, '": "')
+        urlData = urlData.replace(/&/g, '", "')
+        urlData = `{"${urlData}"}`
+        return JSON.parse(urlData)
     }
 }
-/**
- * Contêm métodos que salvam dados na local e na session Storages
- */
+
 const set = {
     Local(key, value) {
         localStorage.setItem(key, JSON.stringify(value));
@@ -95,11 +47,6 @@ const set = {
     }
 }
 
-/**
- * 
- * @param {"input", "onkeyup"} event evento que será ouvido pela função
- * @param {() => {} } func função que sera executada quando o evento for ouvido na página
- */
 const listen = (event, func) => {
     d.addEventListener(event, func);
 }
