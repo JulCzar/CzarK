7aa9afd9f53db8f2bffc4d37ece19ab8f19fe439
JulCZar <julczar21@outlook.com>
2020-05-01 20:47:12 -0300

    Semantic update #2

--- index.js
+++ index.js
@@ -2,33 +2,65 @@
  * JulCzar javascript framework
  * Made by Júlio César Barcelo Monteiro
  * Date: 12/19/2018
- * Last Edit: 04/11/2019
+ * Last Edit: 05/01/2019
  */
 
 const d = document;
 
 const get = {
+    /**
+     * Find an Id in the DOM
+     * @param {String} item Id you want to be located in the DOM
+     */
     Id(item) {
         return d.getElementById(item);
     },
+    /**
+     * Find, in the DOM, all Classes that match
+     * @param {String} item Class you want to locate in the DOM
+     */
     Classes(item) {
         return d.getElementsByClassName(item);
     },
+    /**
+     * Find the first element that match with the query specified
+     * @param {String} item Query you want to locate
+     */
     Query(item) {
         return d.querySelector(item);
     },
+    /**
+     * Find all elements that match with the query specified
+     * @param {String} item Query you want to locate
+     */
     Queries(item) {
         return d.querySelectorAll(item);
     },
+    /**
+     * Gives you the current time in miliseconds
+     */
     Date() {
         return new Date().getTime();
     },
-    Session(id) {
-        return JSON.parse(sessionStorage.getItem(id));
+    /**
+     * Find in the SessionStorage the key requested and return the value parsed
+     * @param {String} key
+     * @returns {JSON}
+     */
+    Session(key) {
+        return JSON.parse(sessionStorage.getItem(key));
     },
-    Local(id) {
-        return JSON.parse(localStorage.getItem(id));
+    /**
+     * Find in the LocalStorage the key requested and return the value parsed
+     * @param {String} key
+     * @returns {JSON}
+     */
+    Local(key) {
+        return JSON.parse(localStorage.getItem(key));
     },
+    /** 
+     * Find all the queries in the URL and return an Object with it.
+     */
     UrlData() {
         const urlParams = new URLSearchParams(location.search);
         let result = new Object()
@@ -40,36 +72,67 @@ const get = {
 }
 
 const set = {
+    /**
+     * Save in the LocalStorage any value you want to
+     * @param {String} key 
+     * @param {*} value 
+     */
     Local(key, value) {
         localStorage.setItem(key, JSON.stringify(value));
     },
+    /**
+     * Save in the SessionStorage any value you want to
+     * @param {String} key 
+     * @param {*} value 
+     */
     Session(key, value) {
         sessionStorage.setItem(key, JSON.stringify(value));
     }
 }
 
+/**
+ * Adds an event listener to a target in the DOM
+ * @param {String} event Event that will be listened
+ * @param {Function} func Function that will be executed when the event is located
+ * @param {Element} target Target. (default = document)
+ */
 const listen = (event, func, target = d) => {
     target.addEventListener(event, func);
 }
 
 const del = {
+    /**
+     * Delete from LocalStorage a key
+     * @param {String} key 
+     */
     fromLocal(key) {
         localStorage.removeItem(key)
     },
+    /**
+     * Delete from SessionStorage a key
+     * @param {String} key 
+     */
     fromSession(key) {
         sessionStorage.removeItem(key)
     },
-    fromArray(arr, elem) {
-        arr.pull(elem)
-    },
-    element(item) {
-        item.remove()
+    /**
+     * Remove from the DOM an element
+     * @param {Element} elem 
+     */
+    element(elem) {
+        elem.remove()
     }
 }
 
-const truncate = (str, maxLength) => {
+/**
+ * Compare the length of a String with a specified value and return it truncated if (str.length > maxLength),
+ * @param {String} str string you want to analize
+ * @param {Number} maxLength pos you want to cut the string if it is higher than
+ * @param {*} sufix will be added at the end of the truncated string
+ */
+const truncate = (str, maxLength, sufix = '...') => {
     if (str.length > maxLength) {
-        str = `${str.substring(0, --maxLength)}...`
+        str = str.substring(0, --maxLength) + sufix
     }
     return str
 }
